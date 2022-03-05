15b6a5b9e87b8e25fb6dfddae8a7771fe9eef126
yama525 <myy.yama1008@gmail.com>
2022-03-05 16:27:56 +0900

    全ページへ SCSS 適用

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('scss/main.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/product_detail.blade.php
+++ resources/views/product_detail.blade.php
@@ -1,14 +1,5 @@
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
     <x-app-layout>
 
         <h1>{{$product->name}}</h1>
@@ -16,5 +7,3 @@
     
     
     </x-app-layout>
-</body>
-</html>
